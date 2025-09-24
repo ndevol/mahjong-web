119388138a3854550517aa87990b6e6e4c8e2329
nsdevol <nsdevol@gmail.com>
2025-09-23 18:53:23 -0700

    Ready player 1.

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,23 @@ function App() {
   const [hands, setHands] = useState([]);
   const [remaining, setRemaining] = useState([]);
 
-  // const startGame = async () => {
-  //   try {
-  //     const res = await axios.get("http://127.0.0.1:8000/start_game");
-  //     setHands(res.data.hands);
-  //     setRemaining(res.data.remaining_tiles);
-  //   } catch (err) {
-  //     console.error("Error starting game:", err);
-  //   }
-  // };
+  const startGame = async () => {
+    try {
+      const res = await axios.get("http://127.0.0.1:8000/start_game");
+      setHands(res.data.hands);
+      setRemaining(res.data.remaining_tiles);
+    } catch (err) {
+      console.error("Error starting game:", err);
+    }
+  };
 
-  // if (hands.length === 0) {
-  //   return (
-  //     <div className="start-screen">
-  //       <button className="start-button" onClick={startGame}>Start Game</button>
-  //     </div>
-  //   );
-  // }
+  if (hands.length === 0) {
+    return (
+      <div className="start-screen">
+        <button className="start-button" onClick={startGame}>Start Game</button>
+      </div>
+    );
+  }
 
   return (
     <div className="app-container">
@@ -46,7 +46,11 @@ function App() {
             </div>
           </div>
           <div className="horizontal-player-container">
-            <div className="hand bottom"></div>
+            <div className="hand bottom">
+              {hands[0].map((tile, i) => (
+                <Tile key={i} tile={tile} faceUp={true} />
+              ))}
+            </div>
             <div className="pairs bottom"></div>
           </div>
         </div>
