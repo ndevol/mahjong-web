d523f0fa0405ee43da7528a90c673d814b18224c
nsdevol <nsdevol@gmail.com>
2025-09-26 22:49:01 -0700

    Switch to grid

--- src/App.jsx
+++ src/App.jsx
@@ -40,44 +40,38 @@ function App() {
   return (
     <div className="app-container">
       <div className="header"></div>
-      <div className="game-container">
-        <div className="vertical-flex">
-          <div className="horizontal-player-container">
-            <div className="hand top">
-              {playerHands.top.map((tile, i) => (
-                <Tile key={i} tile={tile} faceUp={false} />
-              ))}
-            </div>
-            <div className="pairs top"></div>
-          </div>
-          <div className="middle-flex">
-            <div className="vertical-player-container">
-              <div className="hand left">
-                {/* {playerHands.left.map((tile, i) => (
-                  <Tile key={i} tile={tile} faceUp={false} />
-                ))} */}
-              </div>
-              <div className="pairs left"></div>
-            </div>
-            <div className="middle"></div>
-            <div className="vertical-player-container">
-              <div className="hand right">
-                {/* {playerHands.right.map((tile, i) => (
-                  <Tile key={i} tile={tile} faceUp={false} />
-                ))} */}
-              </div>
-              <div className="pairs right"></div>
-            </div>
-          </div>
-          <div className="horizontal-player-container">
-            <div className="hand bottom">
-              {playerHands.bottom.map((tile, i) => (
-                <Tile key={i} tile={tile} faceUp={true} />
-              ))}
-            </div>
-            <div className="pairs bottom"></div>
-          </div>
+      <div className="left-player-container">
+        <div className="hand left">
+          {/* {playerHands.left.map((tile, i) => (
+            <Tile key={i} tile={tile} faceUp={false} />
+          ))} */}
         </div>
+        <div className="pairs left"></div>
+      </div>
+      <div className="top-player-container">
+        <div className="hand top">
+          {/* {playerHands.top.map((tile, i) => (
+            <Tile key={i} tile={tile} faceUp={false} />
+          ))} */}
+        </div>
+        <div className="pairs top"></div>
+      </div>
+      <div className="middle"></div>
+      <div className="right-player-container">
+        <div className="hand right">
+          {/* {playerHands.right.map((tile, i) => (
+            <Tile key={i} tile={tile} faceUp={false} />
+          ))} */}
+        </div>
+        <div className="pairs right"></div>
+      </div>
+      <div className="bottom-player-container">
+        <div className="hand bottom">
+          {/* {playerHands.bottom.map((tile, i) => (
+            <Tile key={i} tile={tile} faceUp={true} />
+          ))} */}
+        </div>
+        <div className="pairs bottom"></div>
       </div>
     </div>
   )
